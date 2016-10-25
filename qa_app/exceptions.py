ad3f26e2fa27ff5d9479e58d2c609d4665c3c03a
Alexey Galkin <leks.molecul@gmail.com>
2016-10-25 14:05:49 +0300

    Fix error templates

--- qa_app/exceptions.py
+++ qa_app/exceptions.py
@@ -17,10 +17,10 @@ from flask import current_app, render_template
 
 @current_app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 
 @current_app.errorhandler(Exception)
 def internal_error(e):
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
 
